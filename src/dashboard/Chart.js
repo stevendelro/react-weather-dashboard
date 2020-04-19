76256b46ddc99328cea35b1c3f258ef0921fdcfd
Steven Del Rosario <stevendelro@pm.me>
2020-04-19 14:26:42 -0700

    Chart displays daily hi/lo temps

--- src/dashboard/Chart.js
+++ src/dashboard/Chart.js
@@ -1,54 +1,87 @@
-import React from 'react';
-import { useTheme } from '@material-ui/core/styles';
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
-import Title from './Title';
+import React, { useState, useEffect } from 'react'
+import { useTheme } from '@material-ui/core/styles'
+import Title from './Title'
+import {
+  Area,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+  Tooltip,
+  Bar,
+  ComposedChart,
+  Legend,
+} from 'recharts'
 
-// Generate Sales Data
-function createData(time, amount) {
-  return { time, amount };
-}
+export default function Chart({ state }) {
+  const theme = useTheme()
+  const [chartData, setChartData] = useState([])
 
-const data = [
-  createData('00:00', 0),
-  createData('03:00', 300),
-  createData('06:00', 600),
-  createData('09:00', 800),
-  createData('12:00', 1500),
-  createData('15:00', 2000),
-  createData('18:00', 2400),
-  createData('21:00', 2400),
-  createData('24:00', undefined),
-];
+  const CustomTooltip = ({ active, payload }) => {
+    if (active) {
+      return (
+        <div>
+          <h2>{payload[0].payload.more.date}</h2>
+          <hr />
+          <p>High: {payload[0].payload.more.apparentTemperatureHigh.toFixed(0)}°F</p>
+          <p>Low: {payload[0].payload.more.apparentTemperatureLow.toFixed(0)}°F</p>
+          {/* <p>Range: {payload[0].temperature[0]}°F - {payload[0].temperature[1]}°F </p> */}
+          <p>Chance of rain: {payload[0].payload.more.precipProbability * 100}%</p>
+          <p>{payload[0].payload.more.summary}</p>
+        </div>
+      );
+    }
+  
+    return null;
+  };
 
-export default function Chart() {
-  const theme = useTheme();
+  useEffect(() => {
+    state.weather.daily.data.forEach(day => {
+      setChartData(prev => [
+        ...prev,
+        {
+          weekday: day.weekday,
+          date: day.date,
+          lo: day.apparentTemperatureLow.toFixed(0),
+          hi: day.apparentTemperatureHigh.toFixed(0),
+          temperature: [
+            day.apparentTemperatureLow.toFixed(0),
+            day.apparentTemperatureHigh.toFixed(0),
+          ],
+          more: day
+        }
+      ])
+    })
+  }, [state.weather.daily.data])
 
   return (
-    <React.Fragment>
-      <Title>Today</Title>
+    <>
+      <Title>Daily Forcast</Title>
       <ResponsiveContainer>
-        <LineChart
-          data={data}
+        <ComposedChart
+          data={chartData}
           margin={{
             top: 16,
             right: 16,
             bottom: 0,
             left: 24,
-          }}
-        >
-          <XAxis dataKey="time" stroke={theme.palette.text.secondary} />
-          <YAxis stroke={theme.palette.text.secondary}>
-            <Label
-              angle={270}
-              position="left"
-              style={{ textAnchor: 'middle', fill: theme.palette.text.primary }}
-            >
-              Sales ($)
-            </Label>
-          </YAxis>
-          <Line type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={false} />
-        </LineChart>
+          }}>
+          <XAxis dataKey='weekday' />
+          <YAxis unit='°F' />
+          <CartesianGrid strokeDasharray='3 3' />
+          <Bar dataKey='hi' name='High' fill='#ffe2e2' unit='°F' barSize={20} />
+          <Bar dataKey='lo' name='Low' fill='#bad7df' unit='°F' barSize={20} />
+          <Area
+            dataKey='temperature'
+            name='Temp Range'
+            unit='°F'
+            stroke='#99ddcc'
+            fill='#99ddcc'
+          />
+          <Tooltip content={<CustomTooltip/>}/>
+          <Legend />
+        </ComposedChart>
       </ResponsiveContainer>
-    </React.Fragment>
-  );
+    </>
+  )
 }

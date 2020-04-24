83f443280ef1fa8f502c0f9034c1fb44be280f67
Steven Del Rosario <stevendelro@pm.me>
2020-04-23 20:04:39 -0700

    Added svg weather icons

---
+++ src/icons/icons/ClearDay.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function ClearDay() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="70"
+      height="70"
+      x="0"
+      y="0"
+      version="1.1"
+      viewBox="0 0 70 70"
+      xmlSpace="preserve"
+    >
+      <g>
+        <circle cx="35" cy="35" r="23.5" fill="#FFE499"></circle>
+      </g>
+      <g>
+        <circle cx="35" cy="35" r="18.1" fill="#FFD766"></circle>
+      </g>
+      <circle cx="35" cy="35" r="13.6" fill="#FFBC00"></circle>
+    </svg>
+  );
+}
+
+export default ClearDay;

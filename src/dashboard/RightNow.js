32c83a3997799dfb126d50556dc6c3f37b45a3b3
Steven Del Rosario <stevendelro@pm.me>
2020-04-25 10:23:34 -0700

    SVG within RightNow is now layout responsive.

--- src/dashboard/RightNow.js
+++ src/dashboard/RightNow.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import serveIcon from '../icons/index'
 
 const useStyles = makeStyles({
-  summaryText: {
-    flex: 1,
+
+  iconBox: {
+    width: '60%',
   },
-  tempText: {
-    marginTop: '1rem',
+  todayDate: {
+    marginTop: '20px',
   },
 })
 
@@ -15,32 +19,26 @@ export default function RightNow({ state }) {
   const classes = useStyles()
   return (
     <React.Fragment>
-      <Typography
-        align='center'
-        component='h2'
-        variant='h6'
-        color='primary'
-        gutterBottom>
-        {state.location.shortName
-          ? state.location.shortName.toUpperCase()
-          : state.location.searchedTerm.toUpperCase()}
-      </Typography>
-      <Typography
-        className={classes.tempText}
-        align='center'
-        component='p'
-        variant='h2'>
-        {Number(state.weather.currently.temperature).toFixed(0)}°F
-      </Typography>
-      <Typography
-        align='center'
-        color='textSecondary'
-        className={classes.summaryText}>
-        {state.weather.hourly.summary}
-      </Typography>
-      <Typography align='center' variant='subtitle2' color='textSecondary'>
-        {state.weather.currently.today}
-      </Typography>
+      <Grid container direction='column' justify='center' alignItems='center'>
+        <Typography align='center' component='h2' variant='h6' color='primary'>
+          {state.weather.currently.today}
+        </Typography>
+
+        <Box className={classes.iconBox}>
+          {serveIcon(state.weather.currently.icon)}
+        </Box>
+
+        <Typography align='center' component='p' variant='h2'>
+          {Number(state.weather.currently.temperature).toFixed(0)}°F
+        </Typography>
+        <Typography
+          align='center'
+          color='textSecondary'
+          gutterBottom>
+          {state.weather.hourly.summary}
+        </Typography>
+
+      </Grid>
     </React.Fragment>
   )
 }
--- src/icons/icons/ClearDay.js
+++ src/icons/icons/ClearDay.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React from 'react'
+import styled from 'styled-components'
 
 function ClearDay() {
+  const ResponsiveSVGContainer = styled.div`
+	display: inline-block;
+	position: relative;
+	width: 100%;
+	padding-bottom: 100%; 
+	vertical-align: middle; 
+	overflow: hidden; 
+`
+
+const ResponsiveSVGContent = styled.svg`
+  display: inline-block;
+	position: absolute;
+	top: 0;
+	left: 0;
+`
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="70"
-      height="70"
-      x="0"
-      y="0"
-      version="1.1"
-      viewBox="0 0 70 70"
-      xmlSpace="preserve"
-    >
-      <g>
-        <circle cx="35" cy="35" r="23.5" fill="#FFE499"></circle>
-      </g>
-      <g>
-        <circle cx="35" cy="35" r="18.1" fill="#FFD766"></circle>
-      </g>
-      <circle cx="35" cy="35" r="13.6" fill="#FFBC00"></circle>
-    </svg>
-  );
+    <ResponsiveSVGContainer>
+      <ResponsiveSVGContent
+        xmlns='http://www.w3.org/2000/svg'
+        width='100%'
+        height='100%'
+        preserveAspectRatio="xMinYMin meet"
+        x='0'
+        y='0'
+        version='1.1'
+        viewBox='0 0 70 70'
+        xmlSpace='preserve'>
+        <g>
+          <circle cx='35' cy='35' r='23.5' fill='#FFE499'></circle>
+        </g>
+        <g>
+          <circle cx='35' cy='35' r='18.1' fill='#FFD766'></circle>
+        </g>
+        <circle cx='35' cy='35' r='13.6' fill='#FFBC00'></circle>
+      </ResponsiveSVGContent>
+    </ResponsiveSVGContainer>
+  )
 }
 
-export default ClearDay;
+export default ClearDay
